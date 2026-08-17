c15f106e696a1e709a5c7c1835ea98ca657ac1e2
ayong <007ayongsvip@gmail.com>
2026-08-17 22:06:55 +0800

    Add neat-download-manager cask (v1.3)

---
+++ Cask/neat-download-manager.rb
@@ -0,0 +1,17 @@
+cask "neat-download-manager" do
+  version "1.3"
+  sha256 "c0e301ee4b13504a76eb6c54846c9dcf6d8148d32489b4bb91bd2062d471ef81"
+
+  url "https://www.neatdownloadmanager.com/file/NeatDMInstaller.dmg"
+  name "Neat Download Manager"
+  name "NDM"
+  desc "Multi-threaded download manager for macOS"
+  homepage "https://www.neatdownloadmanager.com/index.php"
+
+  app "NeatDownloadManager.app"
+
+  zap trash: [
+    "~/Library/Application Support/NeatDownloadManager",
+    "~/Library/Preferences/com.NeatDownloadManager.plist",
+  ]
+end
