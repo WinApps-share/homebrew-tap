76cbede84320f8ac45f5da3b8d851fe2efb47e6d
ayong <007ayongsvip@gmail.com>
2026-08-17 22:18:36 +0800

    Fix cask style violations and bump actions/cache to v4

--- Cask/neat-download-manager.rb
+++ Cask/neat-download-manager.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 cask "neat-download-manager" do
   version "1.3"
   sha256 "c0e301ee4b13504a76eb6c54846c9dcf6d8148d32489b4bb91bd2062d471ef81"
@@ -5,7 +7,7 @@ cask "neat-download-manager" do
   url "https://www.neatdownloadmanager.com/file/NeatDMInstaller.dmg"
   name "Neat Download Manager"
   name "NDM"
-  desc "Multi-threaded download manager for macOS"
+  desc "Multi-threaded download manager"
   homepage "https://www.neatdownloadmanager.com/index.php"
 
   app "NeatDownloadManager.app"
