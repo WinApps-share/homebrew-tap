d746ec1a2923a67c6353677f3b73cc1f79d02b9b
ayong <007ayongsvip@gmail.com>
2026-08-22 20:27:39 +0800

    ecopaste: Add version 1.1.0

---
+++ Casks/ecopaste.rb
@@ -0,0 +1,28 @@
+cask "ecopaste" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "1.1.0"
+  sha256 arm:   "cfdeaaeb92c59903db801c0d12901424c8e4f14d6e2bba40a1f83384c3224c1b",
+         intel: "a431d5cc02e63f6172a8da1606ffef16dfb75a30c2e0ba3d928061154742ac0d"
+
+  url "https://github.com/EcoPasteHub/EcoPaste/releases/download/v#{version}/EcoPaste_#{version}_#{arch}.dmg"
+  name "EcoPaste"
+  desc "Cross-platform clipboard management tool"
+  homepage "https://github.com/EcoPasteHub/EcoPaste"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :big_sur
+
+  app "EcoPaste.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.ayangweb.eco-paste",
+    "~/Library/Caches/com.ayangweb.eco-paste",
+    "~/Library/Logs/com.ayangweb.eco-paste",
+    "~/Library/WebKit/com.ayangweb.eco-paste",
+  ]
+end
