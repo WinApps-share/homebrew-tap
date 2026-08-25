827773e969dedb488d7b6f40d4700acaf043f267
ayong <007ayongsvip@gmail.com>
2026-08-25 14:09:06 +0800

    sideloadly: Add version 0.60.0

---
+++ Casks/sideloadly.rb
@@ -0,0 +1,28 @@
+cask "sideloadly" do
+  version "0.60.0"
+  sha256 "428d062af1ca819712fb12cb0ace25fa49c80d9735c73cda3cbaf09ffcd63212"
+
+  url "https://sideloadly.io/SideloadlySetup.dmg?version=#{version}"
+  name "Sideloadly"
+  desc "Sideload IPA files to iOS devices"
+  homepage "https://sideloadly.io/"
+
+  livecheck do
+    url "https://sideloadly.io/#changelog"
+    regex(%r{v(\d+(?:\.\d+)+)</span>}i)
+  end
+
+  depends_on macos: :monterey
+
+  app "Sideloadly.app"
+
+  zap trash: [
+    "/Users/ayong/Library/Application Support/CrashReporter/Sideloadly_32C71AA3-0CFC-5D9F-8204-620ECACFC378.plist",
+    "/Users/ayong/Library/LaunchAgents/io.sideloadly.daemon.plist",
+    "/Users/ayong/Library/Preferences/io.sideloadly.sideloadly.plist",
+  ]
+
+  caveats do
+    requires_rosetta
+  end
+end
