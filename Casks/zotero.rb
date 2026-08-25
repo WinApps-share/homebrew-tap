2e9f878f16fb881722994abe5b67099e1c3dfe4b
ayong <007ayongsvip@gmail.com>
2026-08-25 22:40:00 +0800

    zotero: Add version 10.0.1

---
+++ Casks/zotero.rb
@@ -0,0 +1,31 @@
+cask "zotero" do
+  version "10.0.1"
+  sha256 "1b6e57cedb2dbcbe3f31c06069734d2201559da99a34c7f84167b6b4c7ee5aa8"
+
+  url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}.dmg"
+  name "Zotero"
+  desc "Collect, organise, cite, and share research sources"
+  homepage "https://www.zotero.org/"
+
+  livecheck do
+    url "https://www.zotero.org/download/client/dl?channel=release&platform=mac"
+    strategy :header_match
+  end
+
+  auto_updates true
+  depends_on :macos
+
+  app "Zotero.app"
+
+  uninstall quit: "org.zotero.zotero"
+
+  zap trash: [
+        "~/Library/Application Scripts/org.zotero.SafariExtensionApp.SafariExtension",
+        "~/Library/Application Support/Zotero",
+        "~/Library/Caches/Zotero",
+        "~/Library/Containers/org.zotero.SafariExtensionApp.SafariExtension",
+        "~/Library/Preferences/org.zotero.zotero.plist",
+        "~/Library/Saved Application State/org.zotero.zotero.savedState",
+      ],
+      rmdir: "~/Zotero"
+end
