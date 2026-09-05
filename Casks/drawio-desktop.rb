62574f848cb76538141a3812ee16c9b96070f9b1
ayong <007ayongsvip@gmail.com>
2026-09-05 23:11:13 +0800

    Revert "drawio-desktop: Update minimum macOS version"
    This reverts commit fb91b90911afe90135d2ee43c0dcfc613c17b116.

--- Casks/drawio-desktop.rb
+++ Casks/drawio-desktop.rb
@@ -5,7 +5,8 @@ cask "drawio-desktop" do
   sha256 arm:   "b6eda7e82bc09454274c03533bce30de170dfdee278ddf5e3c966d021fa16aee",
          intel: "13f12dddcf9960fb3d3783f2f6498ec5e7b93e7490926167b0590a6bd66d5cd2"
 
-  url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{arch}-#{version}.dmg"
+  url "https://github.com/jgraph/drawio-desktop/releases/download/v#{version}/draw.io-#{arch}-#{version}.dmg",
+      verified: "github.com/jgraph/drawio-desktop/"
   name "draw.io"
   desc "Official electron build of draw.io"
   homepage "https://www.diagrams.net/"
@@ -15,7 +16,8 @@ cask "drawio-desktop" do
     strategy :github_latest
   end
 
-  depends_on macos: :ventura
+  depends_on macos: :monterey
+
   app "draw.io.app"
 
   zap trash: [
