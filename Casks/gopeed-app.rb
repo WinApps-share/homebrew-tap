e5a472dae9eccdb4d6d1bf66f6a17716f73f05d5
ayong <007ayongsvip@gmail.com>
2026-09-06 13:10:22 +0800

    gopeed: 移除应用的 URL 验证并更新 macOS 依赖

--- Casks/gopeed-app.rb
+++ Casks/gopeed-app.rb
@@ -5,8 +5,7 @@ cask "gopeed-app" do
   sha256 arm:   "7e1b6c8e9270d99e0d0238efbd53072b58847ffd71d13c3cc8cf203f99c4769e",
          intel: "fdb6be465be15765c953731e83c48a9e2bb3edf0eff3acd3162b80f94df30cac"
 
-  url "https://github.com/GopeedLab/gopeed/releases/download/v#{version}/Gopeed-v#{version}-macos-#{arch}.dmg",
-      verified: "github.com/GopeedLab/gopeed/"
+  url "https://github.com/GopeedLab/gopeed/releases/download/v#{version}/Gopeed-v#{version}-macos-#{arch}.dmg"
   name "Gopeed"
   desc "跨平台的 Gopeed 多线程下载器"
   homepage "https://gopeed.com/"
@@ -16,7 +15,7 @@ cask "gopeed-app" do
     strategy :github_latest
   end
 
-  depends_on macos: :catalina
+  depends_on :macos
 
   app "Gopeed.app"
 
