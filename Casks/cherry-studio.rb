8d4786f742901bdee72a8737f459b72fc14a550a
ayong <007ayongsvip@gmail.com>
2026-09-19 19:37:05 +0800

    cherry-studio: Update to version 2.1.0

--- Casks/cherry-studio.rb
+++ Casks/cherry-studio.rb
@@ -1,9 +1,9 @@
 cask "cherry-studio" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.0.14"
-  sha256 arm:   "955690c2362557ddda21d66491661910be0d58aec5712945bb5b6c6ad51304b3",
-         intel: "bc54e59eb9b84f3db2bd67c3752c965124978df400d8be25e1beff5c5251c649"
+  version "2.1.0"
+  sha256 arm:   "f2a394c2da6f72eef16b844280b4173712429c9a763fe90a7e5f606df98ac27f",
+         intel: "a00430b0a15447a6964df3304a4b5b1968abcc68b6decec97849ddedde368f7d"
 
   url "https://github.com/CherryHQ/cherry-studio/releases/download/v#{version}/Cherry-Studio-#{version}-mac-#{arch}.dmg"
   name "Cherry Studio"
@@ -16,7 +16,7 @@ cask "cherry-studio" do
   end
 
   auto_updates true
-  depends_on macos: :monterey
+  depends_on macos: :ventura
 
   app "Cherry Studio.app"
 
