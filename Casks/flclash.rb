238aa077105e08e39769bacd4658d2d9d7d2776a
ayong <007ayongsvip@gmail.com>
2026-09-19 13:58:21 +0800

    flclash@0.8.98: Fix hash error

--- Casks/flclash.rb
+++ Casks/flclash.rb
@@ -2,7 +2,7 @@ cask "flclash" do
   arch arm: "arm64", intel: "amd64"
 
   version "0.8.98"
-  sha256 arm:   "6d2b5bf9a9f41bfb6fb449fe73718ee42356bef1aac6a7b343ff7483d4a70dc8",
+  sha256 arm:   "daaa8449f6b0e67ced7aa0472df2f15befbd5ed5944f36da4f90b0051a38ddb1",
          intel: "d72a5aea55e8514f742ab9567016f2e7d4c44e8c190012e291997e42c876c5e1"
 
   url "https://github.com/chen08209/FlClash/releases/download/v#{version}/FlClash-#{version}-macos-#{arch}.dmg"
